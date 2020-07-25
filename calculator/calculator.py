08dd998b2f0b0f461072514be7497856c8ad1b26
rabbani191 <umar.rabbani.191@gmail.com>
2020-07-25 04:34:14 -0500

    reviewed issues resolved

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,20 +1,25 @@
-print("Select Operation:\n1- Add,\n2- Subtract,\n3- Multiply,\n4- Divide.");
-option = int(input("Enter you choice from 1/2/3/4 :- "));
-num1 = input("Enter your first number :- ");
-num2 = input("Enter your second number :- ");
+def add(a, b):
+    return int(a) + int(b)
+def subtract(a, b):
+    return int(a) - int(b)
+def multiply(a, b):
+    return int(a) * int(b)
+def divide(a, b):
+    return int(a) / int(b)
+
+print("Select Operation:\n1- Add,\n2- Subtract,\n3- Multiply,\n4- Divide.")
+option = int(input("Enter you choice from 1/2/3/4 :- "))
+num1 = input("Enter your first number :- ")
+num2 = input("Enter your second number :- ")
 if option == 1:
-    {
-        print("results : ", int(num2)+int(num1))
-    }
+        print("results : ", add(num1,num2))
+
 elif option == 2:
-    {
-        print("results : ", int(num1)-int(num2))
-    }
+        print("results : ", subtract(num1,num2))
+
 elif option == 3:
-    {
-        print("results : ", int(num1)*int(num2))
-    }
+        print("results : ", multiply(num1,num2))
+
 elif option == 4:
-    {
-        print("results : ", int(num1) / int(num2))
-    }
+        print("results : ", divide(num1,num2))
+
