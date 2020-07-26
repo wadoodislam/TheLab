03a104f0c3588589e7db95002bf82d61991bd586
rabbani191 <umar.rabbani.191@gmail.com>
2020-07-26 12:49:17 -0500

    Calculator Updated

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -5,21 +5,19 @@ def subtract(a, b):
 def multiply(a, b):
     return int(a) * int(b)
 def divide(a, b):
-    return int(a) / int(b)
+    return float(a) / float(b)
 
-print("Select Operation:\n1- Add,\n2- Subtract,\n3- Multiply,\n4- Divide.")
-option = int(input("Enter you choice from 1/2/3/4 :- "))
-num1 = input("Enter your first number :- ")
-num2 = input("Enter your second number :- ")
-if option == 1:
-        print("results : ", add(num1,num2))
-
-elif option == 2:
-        print("results : ", subtract(num1,num2))
-
-elif option == 3:
-        print("results : ", multiply(num1,num2))
-
-elif option == 4:
-        print("results : ", divide(num1,num2))
+print("Calculator");
+num1 = input("First Number: ")
+option = input("Operator (+, -, *, /): ")
+num2 = input("Second Number: ")
+if option == "+":
+        result = add(num1,num2)
+elif option == "-":
+        result =  subtract(num1,num2)
+elif option == "*":
+        result = multiply(num1,num2)
+elif option == "/":
+        result = divide(num1,num2)
+print("Results: ",result)
 
