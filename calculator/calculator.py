93b5172700d23bedfba337767ae8e690ce52ea60
rabbani191 <umar.rabbani.191@gmail.com>
2020-07-26 15:25:32 -0500

    Updated calculator with loops

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,23 +1,34 @@
-def add(a, b):
-    return int(a) + int(b)
-def subtract(a, b):
-    return int(a) - int(b)
-def multiply(a, b):
-    return int(a) * int(b)
-def divide(a, b):
-    return float(a) / float(b)
+def calculate(a,b,operator):
+    if operator == "+":
+        result = int(a) + int(b)
+    elif operator == "-":
+        result = int(a) - int(b)
+    elif operator == "*":
+        result = int(a) * int(b)
+    elif operator == "/":
+        result = float(a) / float(b)
+    return result
+
 
 print("Calculator");
-num1 = input("First Number: ")
-option = input("Operator (+, -, *, /): ")
-num2 = input("Second Number: ")
-if option == "+":
-        result = add(num1,num2)
-elif option == "-":
-        result =  subtract(num1,num2)
-elif option == "*":
-        result = multiply(num1,num2)
-elif option == "/":
-        result = divide(num1,num2)
-print("Results: ",result)
+count = 0
+while count == 0:
+    num1 = input("First Number: ")
+    option = input("Operator (+, -, *, /, =): ")
+    if option == "=":
+        print("Results: ", num1)
+        break
+    num2 = input("Second Number: ")
+    results = calculate(num1,num2,option)
+    print("Results: ", results)
+    count = count + 1
+while count > 0:
+    option = input("Operator (+, -, *, /, =): ")
+    if option == "=":
+        print("Results: ", results)
+        break
+    num2 = input("Second Number: ")
+    results = calculate(results, num2, option)
+    print("Results: ", results)
+
 
