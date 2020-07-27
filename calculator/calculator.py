947831b8ae5e7300448f2b47226a038ab1d3fddf
rabbani191 <umar.rabbani.191@gmail.com>
2020-07-27 12:16:30 -0500

    Updated

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,29 +1,19 @@
 def calculate(a,b,operator):
     if operator == "+":
-        result = int(a) + int(b)
+        result = float(a) + float(b)
     elif operator == "-":
-        result = int(a) - int(b)
+        result = float(a) - float(b)
     elif operator == "*":
-        result = int(a) * int(b)
+        result = float(a) * float(b)
     elif operator == "/":
         result = float(a) / float(b)
     return result
 
 print("Calculator");
-count = 0
-
-while count == 0:
-    num1 = input("First Number: ")
-    option = input("Operator (+, -, *, /, =): ")
-    if option == "=":
-        print("Results: ", num1)
-        break
-    num2 = input("Second Number: ")
-    results = calculate(num1,num2,option)
-    print("Results: ", results)
-    count = count + 1
-    
-while count > 0:
+num1 = input("First Number: ")
+option = "+"
+results = num1;
+while option != "=":
     option = input("Operator (+, -, *, /, =): ")
     if option == "=":
         print("Results: ", results)
