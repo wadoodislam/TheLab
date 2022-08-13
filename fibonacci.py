59e017475eab788488c9f59bb2c78310b9eeadb2
Aliyan Ali <123aliyanali@gmail.com>
2022-08-13 14:40:39 -0700

    "fibonacci program"

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+def fibonacci(nth, first, second):
+    print("write this function")
+
+    value = first
+    value2 = second
+    print(value2)
+    for i in range(nth):
+        print(value)
+        sum = value+value2
+        value2 = value
+        value = sum
+
+
+fibonacci(6, 1, 0)
+
+
+
